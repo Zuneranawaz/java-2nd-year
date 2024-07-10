ae6c2c2806fe3c83d96c758e6ea571e40dcdaf02
Zuneranawaz <shaikzunara@gmail.com>
2024-07-10 12:04:59 +0530

    Greatestoftwo.java

---
+++ Greatestoftwo.java
@@ -0,0 +1,12 @@
+public class Greatestoftwo {
+	public static void main(String[] args) {
+	int x=11,y=10;
+	if(x==y)
+		System.out.println("both are equal");
+	else if(x>y)
+		System.out.println(x+ "is greater");
+	else
+		System.out.println(y+ "is greater");
+	}
+}
+			
