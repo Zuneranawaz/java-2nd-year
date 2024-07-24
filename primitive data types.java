005786247fcb479b5f68b3e92711cddbf916c27b
Zuneranawaz <shaikzunara@gmail.com>
2024-07-24 11:45:38 +0530

    primitive data types.java

---
+++ primitive data types.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+	public class default {  
+			static byte b;
+			static short s;
+			static int i;
+			static long l;
+			static float f;
+			static double d;
+			static char ch;
+			static boolean bl;
+			public static void main(String[] args) {
+				System.out.println("the default values of all primitive data types are:");
+				System.out.println("Byte:" +b);
+				System.out.println("short:" +s);
+				System.out.println("int:" +i);
+				System.out.println("long:" +l);
+				System.out.println("float:" +f);
+				System.out.println("double:" +d);
+				System.out.println("char:" +ch);
+				System.out.println("boolean:" +bl);
+			}
+}
